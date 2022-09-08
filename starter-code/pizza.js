8d5e94194a565ef636ae962279dc4e247ae551f0
WHYSOSERIOUS1234 <103112614+WHYSOSERIOUS1234@users.noreply.github.com>
2022-09-07 22:11:57 -0700

    pizza.js_update1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,28 @@
 // Write your Pizza Builder JavaScript in this file.
+let $body = $("body")
+let sectionArray = document.getElementsByTagName('pep')
+console.log(sectionArray)
+let count = 0
+function Toggle(){
+$(".btn-pepperonni").click(function(){
+    $(".pep").toggle()
+})
+
+$(".btn-mushrooms").click(function(){
+    $(".mushroom").toggle()
+})
+
+$(".btn-green-peppers").click(function(){
+    $(".green-pepper").toggle()
+})
+
+$(".btn-sauce").click(function() {
+    $(".sauce").toggle()
+})
+
+$(".btn-crust").click(function() {
+    $(".crust-gluten-free").toggle()
+})
+}
+Toggle()
+
